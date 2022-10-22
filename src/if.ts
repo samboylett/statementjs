63417b75e107ce89de8d22fa5cb9a5849cc300f3
Sam Boylett <sam.boylett@optix.co.uk>
2022-10-22 21:23:52 +0100

    jsdoc

--- src/if.ts
+++ src/if.ts
@@ -1,12 +1,28 @@
+/**
+ * An inline if/else statement class, with optional elseifs.
+ */
 export class If<T> {
   readonly truthy: () => boolean;
   readonly value: () => T;
 
+  /**
+   * Create the class
+   *
+   * @param {() => boolean} truthy - The truthy evaluator for the first if
+   * @param {() => T} value - Function to return the value if the statement is truthy
+   */
   constructor(truthy: () => boolean, value: () => T) {
     this.truthy = truthy;
     this.value = value;
   }
 
+  /**
+   * Add an else if to the inline statement
+   *
+   * @param {() => boolean} truthy - The truthy evaluator for the first if
+   * @param {() => T} value - Function to return the value if the statement is truthy
+   * @returns {If<T>}
+   */
   elseIf(truthy: () => boolean, value: () => T): If<T> {
     if (this.truthy()) {
       return this;
@@ -15,10 +31,23 @@ export class If<T> {
     return new If<T>(truthy, value);
   }
 
+  /**
+   * Get the final value from the statement.
+   *
+   * @param {() => T} value - Function to return the value if all previous if statements were falsy
+   * @returns {T}
+   */
   else(value: () => T): T {
     return this.truthy() ? this.value() : value();
   }
 }
 
+/**
+ * Return an instance of an inline if class
+ *
+ * @param {() => boolean} truthy - The truthy evaluator for the first if
+ * @param {() => T} value - Function to return the value if the statement is truthy
+ * @returns {If<T>}
+ */
 export const iif = <T>(truthy: () => boolean, value: () => T) =>
   new If<T>(truthy, value);
