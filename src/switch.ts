64c387ca1a19fa3a943ae5ffe8288da350d3d4c1
Sam Boylett <sam.boylett@optix.co.uk>
2022-10-22 21:26:47 +0100

    jsdoc

--- src/switch.ts
+++ src/switch.ts
@@ -1,3 +1,6 @@
+/**
+ * An inline switch statement with cases and default
+ */
 export class Switch<Compare, Return> {
   #compare: Compare;
   #cases: {
@@ -5,10 +8,22 @@ export class Switch<Compare, Return> {
     valueIf: () => Return;
   }[] = [];
 
+  /**
+   * Create the class
+   *
+   * @param {Compare} compare - The value to compare each case to
+   */
   constructor(compare: Compare) {
     this.#compare = compare;
   }
 
+  /**
+   * Add a case to the switch statement
+   *
+   * @param {() => Compare} compareTo - Function to return the value to compare to
+   * @param {() => Return} valueIf - Function to return the return value if the comparison matches
+   * @returns {Switch<Compare, Return>}
+   */
   case(compareTo: () => Compare, valueIf: () => Return): Switch<Compare, Return> {
     this.#cases.push({
       compareTo,
@@ -18,6 +33,12 @@ export class Switch<Compare, Return> {
     return this;
   }
 
+  /**
+   * Get the final value of the switch statement
+   *
+   * @param {() => Return} valueIf - Function to return the return value if none of the cases matched
+   * @returns {Return}
+   */
   default(valueIf: () => Return): Return {
     for (const switchCase of this.#cases) {
       if (switchCase.compareTo() === this.#compare) {
@@ -29,4 +50,10 @@ export class Switch<Compare, Return> {
   }
 }
 
+/**
+ * Return an instance of the Switch class.
+ *
+ * @param {Compare} compare - Value to compare cases to
+ * @returns {Switch<Compare, Return>}
+ */
 export const iswitch = <Compare, Return>(compare: Compare) => new Switch<Compare, Return>(compare);
